7c837a9e6b60aee85b57ced790431b0c1017e72d
Michael Straus <mstraus@oxy.edu>
2024-07-07 16:19:42 -0400

    Removed doubled add-user function

--- streamlit_multipage/Hello.py
+++ streamlit_multipage/Hello.py
@@ -6,41 +6,6 @@ st.set_page_config(
 
 from dep import *
 
-def insert_user(name, password):
-    run_sql(f"INSERT INTO people (Username, Password, Time_Created) VALUES ('{name}', '{password}', CURRENT_TIMESTAMP );")
-    st.write('Account created!')
-    return name
-
-def add_user(name, password, new_user):
-    '''
-    Returns: name if query is successful, False if not
-    '''
-    if not password:
-        return False
-    res = run_sql(f"SELECT Username, Password FROM people WHERE Username = '{name}'")
-    # st.write(res)
-    if res:
-        res_val = res[-1]
-        if new_user:
-            if res_val['Password'] == password:
-                st.write('Signed in!')
-                return name
-            else:
-                st.write('User with this name already exists.')
-                return False
-        else:
-            if res_val['Password'] == password:
-                st.write('Signed in!')
-                return name
-            else:
-                st.write('Wrong password. Please try again.')
-                return False
-    elif not res:
-        if not new_user:
-            st.write(f"User is not in database. Please make a new account.")
-            return False
-        else:
-            return insert_user(name, password)
 
 st.write('# Sign in to access your files')
 new_user = st.checkbox("Create Account", value=False)
