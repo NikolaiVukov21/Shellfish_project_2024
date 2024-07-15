445f116a9b693ea6fb54ddb5b3343f538818a324
JoshuaEssandoh <155324440+JoshuaEssandoh@users.noreply.github.com>
2024-07-15 10:09:34 -0400

    Add files via upload

---
+++ Website/ReactJs/frontend/src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+import Website from './web_structure/website.js';
+
+function App() {
+
+  return (
+    <div>
+      <Website></Website>
+    </div>
+    
+  );
+}
+
+export default App;
