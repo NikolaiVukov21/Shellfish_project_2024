d908a22f0cb14d82db67538bbb09702b469330c4
JoshuaEssandoh <155324440+JoshuaEssandoh@users.noreply.github.com>
2024-07-19 15:44:39 -0400

    Update Bottom.js

--- Website/ReactJs/frontend/src/web_structure/Bottom.js
+++ Website/ReactJs/frontend/src/web_structure/Bottom.js
@@ -1,4 +1,8 @@
 import '.././css/bottom-footer.css'
+import gull from '.././pics/gull2.png'
+import insta from '.././pics/insta.png'
+import email from '.././pics/mail.png'
+import linkedin from '.././pics/Linkedin.png'
 
 function Bottom() {
     return (
@@ -6,24 +10,24 @@ function Bottom() {
             qq
             <div class="footer">
                 <div class="footer-elements">
-                    <img class="gull-icon" src="./pics/gull2.png"/>
+                    <img class="gull-icon" src={gull}/>
                 </div>
                 <div class="footer-elements">
-                    <form method="get" action="#Homepage">
+                    <form method="get" action="#Homepage" style={{backgroundColor: "black"}}>
                         <button class="footer-links" type="submit">
                             HOME
                         </button>    
                     </form> 
                 </div>
                 <div class="footer-elements">
-                    <form method="get" action="#Homepage">
+                    <form method="get" action="#Homepage" style={{backgroundColor: "black"}}>
                         <button class="footer-links" type="submit">
                             ABOUT
                         </button>    
                     </form> 
                 </div>
                 <div class="footer-elements">
-                    <form method="get" action="#Homepage">
+                    <form method="get" action="#Homepage" style={{backgroundColor: "black"}}>
                         <button class="footer-links" type="submit">
                             PROJECTS
                         </button>    
@@ -33,17 +37,17 @@ function Bottom() {
             <div class="social-links">
                 <div class="social-link-element">
                     <button class="Social-button">
-                        <img class="social-icons" src="./pics\insta.png"/>
+                        <img class="social-icons" src={insta}/>
                     </button>    
                 </div>
                 <div class="social-link-element">
                     <button class="Social-button">
-                        <img class="social-icons" src="./pics\mail.png"/>
+                        <img class="social-icons" src={email}/>
                     </button>    
                 </div>
                 <div class="social-link-element">
                     <button class="Social-button">
-                        <img class="social-icons" src="./pics\Linkedin.png"/>
+                        <img class="social-icons" src={linkedin}/>
                     </button>   
                 </div>
             </div>
